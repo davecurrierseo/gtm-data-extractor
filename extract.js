6cc44694b8fe8c05618bad4c7ae3e588a5daff0d
Dave Currier <dave@vtldesign.com>
2023-11-28 08:06:01 -0500

    adds folder and last edited

--- extract.js
+++ extract.js
@@ -17,32 +17,32 @@ function downloadCSV() {
   if (!table) {
     console.error('Table not found on this page.');
   } else {
-    var data = ["Name,Type,Firing Triggers,Last Edited"]; // Headings
-
+    var data = ["Name,Type,Firing Triggers,Folder,Last Edited"]; // Headings
     var rows = table.querySelectorAll('tr.wd-tag-row');
 
     rows.forEach(function (row) {
-      var rowData = [];
-      var cells = row.querySelectorAll('td');
-
-      cells.forEach(function (cell, index) {
-        if (index === 0) return; // Skip the first column (checkbox/icon)
-
-        var cellData = cell.textContent.trim();
-
-        if (index === 3) { // Firing Triggers column
-          var smallTriggerChips = cell.querySelectorAll('.small-trigger-chip.md-gtm-theme');
-          if (smallTriggerChips.length > 0) {
-            cellData = Array.from(smallTriggerChips, chip => chip.textContent.trim()).join('\n');
+       var rowData = [];
+       var cells = row.querySelectorAll('td');
+    
+       cells.forEach(function (cell, index) {
+          if (index === 0) return; // Skip the first column (checkbox/icon)
+    
+          var cellData = cell.textContent.trim();
+    
+          if (index === 3) { // Firing Triggers column
+             var smallTriggerChips = cell.querySelectorAll('.small-trigger-chip.md-gtm-theme');
+             if (smallTriggerChips.length > 0) {
+                cellData = Array.from(smallTriggerChips, chip => chip.textContent.trim()).join(', ');
+             }
           }
-        }
-        rowData.push(cellData);
-      });
-
-      if (rowData.length > 0) {
-        data.push(rowData.join(',')); // Add row data
-      }
+          rowData.push(cellData);
+       });
+    
+       if (rowData.length > 0) {
+          data.push(rowData.join(',')); // Add row data
+       }
     });
+
 
     // Create and download CSV
     var csvContent = 'data:text/csv;charset=utf-8,' + data.join('\n');
